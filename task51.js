261f2a407cf2395c45cbc9f81a1e9a909a2704dd
vardnazaryan <vardnazaryan16@gmail.com>
2022-12-14 16:12:34 +0400

    second commit

---
+++ task51.js
@@ -0,0 +1,22 @@
+let x = 336
+let m = x % 10
+let tasnavor = parseInt(x/10)% 10
+let haruravor = parseInt(x/100)
+if( tasnavor + haruravor == m){
+    console.log(true)
+}else{
+    console.log(false)
+}
+
+
+let x = 336
+let m = x % 10
+let tasnavor = parseInt(x/10)% 10
+let haruravor = parseInt(x/100)
+let t = true
+if( tasnavor + haruravor == m){
+    console.log(t)
+}else{
+    t = false
+    console.log(t)
+}
