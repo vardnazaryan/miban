842377015643a94a889849f862ae29ebea8b043d
vardnazaryan <vardnazaryan16@gmail.com>
2022-12-15 19:51:47 +0400

    task 39-40

---
+++ task39.js
@@ -0,0 +1,11 @@
+let a = 9970;
+let b = 965;
+let c = 1500;
+let d = 4980;
+let arr = [a, b, c, d]
+arr.sort(function(a, b){
+    return a - b
+}
+)
+console.log(arr)
+
---
+++ task40.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 165;
+let c = 500;
+let d = 30;
+let arr = [a, b, c, d]
+arr.sort(function(a,b){
+   return b - a
+})
+console.log(arr)
